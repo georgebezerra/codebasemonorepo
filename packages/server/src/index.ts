70debdff098d9cfff75cf3b76e639b200594c658
georgebezerra <george.contato@gmail.com>
2020-10-27 12:12:40 -0300

    start server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import * as Koa from 'koa'
+import 'colors'
+
+const app = new Koa()
+
+app.use((ctx, next) => {
+  ctx.body = 'Hello World'
+  return next()
+})
+
+app.listen(3000)
+console.log('Hello World'.blue.bold)
