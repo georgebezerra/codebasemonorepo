993aae083146f2993671fc2270ac222ebd8c9cde
georgebezerra <george.contato@gmail.com>
2020-10-27 16:24:44 -0300

    build add graphql, graphql-api-koa and bodyparser

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,8 +2,10 @@ import * as Koa from 'koa'
 import 'colors'
 
 import { router } from './router'
+import Bodyparser = require('koa-bodyparser')
 
 const app = new Koa()
+app.use(Bodyparser())
 
 app.use((ctx, next) => {
   ctx.body = 'Hello World'
--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -1,9 +1,19 @@
+import { execute } from 'graphql-api-koa'
 import * as Router from '@koa/router'
+
+import { schema } from './graphql'
 
 const router = new Router()
 
 router.get('/hello', ctx => {
   ctx.body = 'hello visitor'
 })
+
+router.post(
+  '/graphql',
+  execute({
+    schema
+  })
+)
 
 export { router }
